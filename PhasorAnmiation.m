e32bae607baad18efec24069edd9201c08bff0f6
Hchan1353 <94099534+Hchan1353@users.noreply.github.com>
2022-11-14 13:42:23 +0800

    Update PhasorAnmiation.m

--- PhasorAnmiation.m
+++ PhasorAnmiation.m
@@ -30,14 +30,14 @@ set(gcf,'Color',[1 1 1]);
 set(gca,'FontSize',12);
 
 xP = t; yP = v;
-plot(xP,yP,'k','LineWidth',1);
+plot(xP,yP,'k','LineWidth',1);  % what is this????
 
 % circle
 p = linspace(0,2*pi,500);
-xc = A .* cos(p);
-yc = A .* sin(p);
+xc = A .* cos(p);  % Horizontal Parameter of Circle, plotting action is at line 59
+yc = A .* sin(p);  % Vertical Parameter of Circle, plotting action is at line 59
 
-   
+for a = 1 : N 
 for c = 1 : N
     hold off
    
@@ -45,35 +45,37 @@ for c = 1 : N
    axis off
    
    
-   plot([0 A*sin(w*t(c))],[0 -A*cos(w*t(c))],'lineWidth',1,'Color',[0 0 1]);
+   plot([0 A*cos(w*t(c))],[0 A*sin(w*t(c))],'lineWidth',1,'Color',[0 0 1]); % Tracking line of circle (Blue)
+   plot([-A 72],[A*sin(w*t(c)) A*sin(w*t(c))],'lineWidth',1,'Color',[0 1 1]); % Reference line
    hold on
-   xP = [0 A*sin(w*t(c))]; yP = [-A*cos(w*t(c)) -A*cos(w*t(c))];
-   plot(xP,yP,'r','LineWidth',3)
+   xP = [A*cos(w*t(c)) A*cos(w*t(c))]; yP = [A*sin(w*t(c)) 0]; % Horizontal Component of Circle (red tracking line)
+   plot(xP,yP,'r','LineWidth',3) 
    
-   xP = [12+t(c) 12+t(c)]; yP = [0 v(c)];
+   xP = [12+t(c) 12+t(c)]; yP = [0 v(c)];% Vertical component of sin wave (red trcking line)
    plot(xP,yP,'r','LineWidth',3);
    
-   xP = [12 12+t(end)]; yP = [0 0];
+   xP = [12 12+t(end)]; yP = [0 0]; %Background horizontal axis of sin wave, [start point, end point]
    plot(xP,yP,'k','LineWidth',1);
       
-   plot(xc,yc,'k','lineWidth',1);
+   plot(xc,yc,'k','lineWidth',1);  % Perimeter of circle
    
-   xP = 12+t; yP = v;
-   plot(xP,yP,'m','LineWidth',1);
+   xP = 12+t; yP = v; % Background sin wave (pink)
+   plot(xP,yP,'m','LineWidth',1); 
    
-   xP = 12+t(1:c); yP = v(1:c);
-   plot(xP,yP,'b','LineWidth',3);
+   xP = 12+t(1:c); yP = v(1:c); % Horizontal component of sin wave (blue trcking line)
+   plot(xP,yP,'b','LineWidth',3); 
    
-   xP = [0 0]; yP = [-A A];
+   xP = [0 0]; yP = [-A A]; % Background vertical coordinate axis of circle
    plot(xP,yP,'k','LineWidth',1);
    
-   xP = [-A A]; yP = [0 0];
+   xP = [-A A]; yP = [0 0];  % Background horizontal coordinate axis of circle
    plot(xP,yP,'k','LineWidth',1);
    
    hold off
    set(gca,'FontSize',12);
    
    axis equal
-   axis on
-   pause(0.01)
+   axis off
+   pause(0.01) % Delay 
+end
 end
